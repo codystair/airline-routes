c4975d78404950ade659f5d6f514fbfb50450090
Cody Stair <ccstair27@gmail.com>
2020-08-01 16:13:51 -0400

    Display raw data in table

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
 import React, { Component } from 'react';
 import './App.css';
+import DATA from './data.js';
 
 class App extends Component {
   render() {
+    const rowElements = DATA.routes.map((route, i) => {
+      return <tr row-id={i}>
+               <td>{route.airline}</td>
+               <td>{route.src}</td>
+               <td>{route.dest}</td>
+             </tr>
+    });
+
     return (
       <div className="app">
         <header className="header">
           <h1 className="title">Airline Routes</h1>
         </header>
         <section>
-          <p>
-            Welcome to the app!
-          </p>
+          <table className="routes-table">
+            <thead>
+              <th>Airline</th>
+              <th>Source Airport</th>
+              <th>Destination Airport</th>
+            </thead>
+            <tbody>
+              {rowElements}
+            </tbody>
+          </table>
         </section>
       </div>
     );
