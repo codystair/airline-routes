7d6dfeab68f18b6e234005ece0c088cca9022642
Cody Stair <ccstair27@gmail.com>
2020-08-01 20:40:30 -0400

    Start adding pagination to table

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
 
 class Table extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentPage: 0,
+    };
+  }
+
+  prevPage = (event) => {
+    event.preventDefault();
+    this.setState({currentPage: this.state.currentPage - 1});
+  }
+
+  nextPage = (event) => {
+    event.preventDefault();
+    this.setState({currentPage: this.state.currentPage + 1});
+  }
+
   render() {
+    const start = this.state.currentPage * 25;
+
     const headers = this.props.columns.map((column, i) => {
       return <th key={i}>{column.name}</th>
     });
-    const bodyRows = this.props.rows.map((row, i) => {
+
+    const bodyRows = this.props.rows.slice(start, start + 25).map((row, i) => {
       return <tr key={i}>
                <td>{this.props.format('airline', row.airline)}</td>
                <td>{this.props.format('src', row.src)}</td>
@@ -13,14 +33,25 @@ class Table extends Component {
              </tr>
     });
 
-    return <table className={this.props.className}>
-      <thead>
-        {headers}
-      </thead>
-      <tbody>
-        {bodyRows}
-      </tbody>
-    </table>
+    return (
+      <div>
+        <table className={this.props.className}>
+          <thead>
+            {headers}
+          </thead>
+          <tbody>
+            {bodyRows}
+          </tbody>
+        </table>
+        <div className="pagination">
+          <p>Showing {start + 1}-{start + 25} of {this.props.rows.length} routes.</p>
+          <p>
+            <button disabled={start === 0} onClick={this.prevPage}>Previous Page</button>
+            <button disabled={(start + 25) === this.props.rows.length} onClick={this.nextPage}>Next Page</button>
+          </p>
+        </div>
+      </div>
+    );
   }
 }
 
